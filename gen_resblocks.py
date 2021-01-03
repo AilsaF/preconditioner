f718243d0177170a52a7e5011998ede0df4dabb8
AilsaF <ailsafangtian@gmail.com>
2021-01-03 01:37:01 -0600

    resblock with 9 layers

--- gen_resblocks.py
+++ gen_resblocks.py
@@ -155,8 +155,7 @@ class DeepBlock2(nn.Module):
         self.c6 = nn.Conv2d(h_ch, h_ch, ksize, padding=pad)
         self.c7 = nn.Conv2d(h_ch, h_ch, ksize, padding=pad)
         self.c8 = nn.Conv2d(h_ch, h_ch, ksize, padding=pad)
-        self.c9 = nn.Conv2d(h_ch, h_ch, ksize, padding=pad)
-        self.c10 = nn.Conv2d(h_ch, out_ch, kernel_size=1, padding=0)
+        self.c9 = nn.Conv2d(h_ch, out_ch, kernel_size=1, padding=0)
         if self.num_classes > 0:
             self.b1 = CategoricalConditionalBatchNorm2d(num_classes, in_ch)
             self.b2 = CategoricalConditionalBatchNorm2d(num_classes, h_ch)
@@ -170,7 +169,6 @@ class DeepBlock2(nn.Module):
             self.b7 = nn.BatchNorm2d(h_ch)
             self.b8 = nn.BatchNorm2d(h_ch)
             self.b9 = nn.BatchNorm2d(h_ch)
-            self.b10 = nn.BatchNorm2d(h_ch)
         if self.learnable_sc:
             self.c_sc = nn.Conv2d(in_ch, out_ch, 1)
     #     self._initialize()
@@ -205,7 +203,6 @@ class DeepBlock2(nn.Module):
         h = self.c7(self.activation(self.b7(h)))
         h = self.c8(self.activation(self.b8(h)))
         h = self.c9(self.activation(self.b9(h)))
-        h = self.c10(self.activation(self.b10(h)))
         if self.learnable_sc:
             return h+self.c_sc(x)
         else:
--- higham_disc_resblocks.py
+++ higham_disc_resblocks.py
@@ -224,11 +224,8 @@ class DeepBlock2(nn.Module):
                                             use_adaptivePC=use_adaptivePC, pclevel=pclevel, diter=diter)
         self.c8 = Higham_norm.spectral_norm(nn.Conv2d(h_ch, h_ch, ksize, padding=pad), n_power_iterations=power_iter,
                                             use_adaptivePC=use_adaptivePC, pclevel=pclevel, diter=diter)
-        self.c9 = Higham_norm.spectral_norm(nn.Conv2d(h_ch, h_ch, ksize, padding=pad), n_power_iterations=power_iter,
+        self.c9 = Higham_norm.spectral_norm(nn.Conv2d(h_ch, out_ch, kernel_size=1, padding=0), n_power_iterations=power_iter,
                                             use_adaptivePC=use_adaptivePC, pclevel=pclevel, diter=diter)
-        self.c10 = Higham_norm.spectral_norm(nn.Conv2d(h_ch, out_ch, kernel_size=1, padding=0), n_power_iterations=power_iter,
-                                            use_adaptivePC=use_adaptivePC, pclevel=pclevel, diter=diter)
-        
         if self.learnable_sc:
             self.c_sc = Higham_norm.spectral_norm(nn.Conv2d(in_ch, out_ch, 1, 1, 0), n_power_iterations=power_iter,
                                                   use_adaptivePC=use_adaptivePC, pclevel=pclevel, diter=diter)
@@ -244,14 +241,13 @@ class DeepBlock2(nn.Module):
         h = self.c6(self.activation(h))
         h = self.c7(self.activation(h))
         h = self.c8(self.activation(h))
-        h = self.c9(self.activation(h))
         # relu before downsample
         h = self.activation(h)
         # downsample
         if self.downsample:
             h = F.avg_pool2d(h, 2)   
         # final 1x1 conv
-        h = self.c10(h)
+        h = self.c9(h)
         return h + self.shortcut(x)
 
     def shortcut(self, x):
