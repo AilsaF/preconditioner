311fb8d6a1939138e177e348c08bf01d01a7f8be
AilsaF <ailsafangtian@gmail.com>
2021-01-21 01:15:22 -0600

    for record debug

--- oniresnet.py
+++ oniresnet.py
@@ -9,125 +9,51 @@ from torch.nn import Parameter
 
 __all__ = ['ResNetDebug', 'resnetDebug18', 'resnetDebug34', 'resnetDebug50', 'resnetDebug101', 'resnetDebug152']
 
-class IdentityModule(torch.nn.Module):
-    def __init__(self, *args, **kwargs):
-        super(IdentityModule, self).__init__()
+# class IdentityModule(torch.nn.Module):
+#     def __init__(self, *args, **kwargs):
+#         super(IdentityModule, self).__init__()
 
-    def forward(self, input: torch.Tensor):
-        return input
+#     def forward(self, input: torch.Tensor):
+#         return input
 
-def _IdentityModule(x, *args, **kwargs):
-    """return first input"""
-    return IdentityModule()
+# def _IdentityModule(x, *args, **kwargs):
+#     """return first input"""
+#     return IdentityModule()
 
 class ONINorm(torch.nn.Module):
     def __init__(self, T=7, norm_groups=1, *args, **kwargs):
         super(ONINorm, self).__init__()
         self.T = T
-        self.norm_groups = norm_groups
-        self.eps = 1e-5
-
-    def matrix_power3(self, Input):
-        B=torch.bmm(Input, Input)
-        return torch.bmm(B, Input)
     
-    def matrix_power32(self, Input):
-        B=torch.mm(Input, Input)
-        return torch.mm(B, Input)
-
-    # def forward(self, weight: torch.Tensor):
-    #     assert weight.shape[0] % self.norm_groups == 0
-    #     Z = weight.view(self.norm_groups, weight.shape[0] // self.norm_groups, -1)  # type: torch.Tensor
-    #     Zc = Z - Z.mean(dim=-1, keepdim=True)
-    #     S = torch.matmul(Zc, Zc.transpose(1, 2))
-    #     eye = torch.eye(S.shape[-1]).to(S).expand(S.shape)
-    #     S = S + self.eps*eye
-    #     norm_S = S.norm(p='fro', dim=(1, 2), keepdim=True)
-    #     S = S.div(norm_S)
-    #     B = [torch.Tensor([]) for _ in range(self.T + 1)]
-    #     B[0] = torch.eye(S.shape[-1]).to(S).expand(S.shape)
-    #     for t in range(self.T):
-    #         B[t + 1] = torch.baddbmm(1.5, B[t], -0.5, self.matrix_power3(B[t]), S)
-    #     W = B[self.T].matmul(Zc).div_(norm_S.sqrt())
-    #     return W.view_as(weight)
-
-    def forward(self, weight: torch.Tensor):
-        Z = weight.view(weight.shape[0], -1)
-        V = Z / Z.norm(p='fro')
-        S = V.mm(V.t())
-        eye = torch.eye(S.shape[-1]).to(S).expand(S.shape)
-        S = S + self.eps*eye
-        B = [torch.Tensor([]) for _ in range(self.T + 1)]
-        B[0] = torch.eye(S.shape[-1]).to(S).expand(S.shape)
-        for t in range(self.T):
-            B[t + 1] = 1.5*B[t]-0.5*torch.mm(self.matrix_power32(B[t]), S)
-        W = torch.mm(B[self.T], V)
-        return W.view_as(weight)
-
-
-    def extra_repr(self):
-        fmt_str = ['T={}'.format(self.T)]
-        if self.norm_groups > 1:
-            fmt_str.append('groups={}'.format(self.norm_groups))
-        return ', '.join(fmt_str)
-
-
-class ONINorm_colum(torch.nn.Module):
-    def __init__(self, T=7, norm_groups=1, *args, **kwargs):
-        super(ONINorm_colum, self).__init__()
-        self.T = T
-        self.norm_groups = norm_groups
-        self.eps = 1e-5
-        #print(self.eps)
-
-    def matrix_power3(self, Input):
-        B=torch.bmm(Input, Input)
-        return torch.bmm(B, Input)
-
-    def forward(self, weight: torch.Tensor):
-        assert weight.shape[0] % self.norm_groups == 0
-        Z = weight.view(self.norm_groups, weight.shape[0] // self.norm_groups, -1)  # type: torch.Tensor
-        Zc = Z - Z.mean(dim=-1, keepdim=True)
-        S = torch.matmul(Zc.transpose(1, 2), Zc)
-        eye = torch.eye(S.shape[-1]).to(S).expand(S.shape)
-        S = S + self.eps*eye
-        norm_S = S.norm(p='fro', dim=(1, 2), keepdim=True)
-        B = [torch.Tensor([]) for _ in range(self.T + 1)]
-        B[0] = torch.eye(S.shape[-1]).to(S).expand(S.shape)
-        for t in range(self.T):
-            B[t + 1] = torch.baddbmm(1.5, B[t], -0.5, self.matrix_power3(B[t]), S)
-        W = Zc.matmul(B[self.T]).div_(norm_S.sqrt())
-        return W.view_as(weight)
-
-    def extra_repr(self):
-        fmt_str = ['T={}'.format(self.T)]
-        if self.norm_groups > 1:
-            fmt_str.append('groups={}'.format(self.norm_groups))
-        return ', '.join(fmt_str)
+    def forward(self, weight):
+        weight_shape = weight.shape
+        weight = weight.view(weight.shape[0], -1)
+        sigma = torch.norm(weight)
+        # sigma = (torch.linalg.norm(weight, float('inf')) * torch.linalg.norm(weight, 1)) ** 0.5
+        if sigma > 0.:
+            weight = weight / sigma
+            # if len(weight.shape) > 2:
+            if self.T:
+                # n, m = weight.shape
+                # if n >= m:
+                #     weight = self.preconditionertall(weight, self.pclevel)
+                # else:
+                #     weight = self.preconditionerwide(weight, self.pclevel)
+                for _ in range(self.T):
+                    weight = 1.5 * weight - 0.5 * weight.mm(weight.t()).mm(weight)
+                weight = weight.view(weight_shape)
+            return weight #* sigma
+        else:
+            return weight.view(weight_shape) #* torch.tensor(1.)
+        
 
 class ONI_Conv2d(torch.nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True,
-                 T=7, norm_groups=1, norm_channels=0, NScale=1.414, adjustScale=False, ONIRow_Fix=False, *args, **kwargs):
+                 T=7, norm_groups=1, norm_channels=0, NScale=1.414, adjustScale=False, ONIRow_Fix=True, *args, **kwargs):
         super(ONI_Conv2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
         print('ONI channels:--OD:',out_channels, '--ID:', in_channels, '--KS',kernel_size)
-        if out_channels <= (in_channels*kernel_size*kernel_size):
-            if norm_channels > 0:
-                norm_groups = out_channels // norm_channels
-            #print('ONI_Conv_Row:--T=', T, '----norm_groups:', norm_groups, '---NScale:', NScale, '---adjust:', adjustScale)
-            self.weight_normalization = ONINorm(T=T, norm_groups=norm_groups)
-        else:
-            if ONIRow_Fix:
-              #  print('ONI_Conv_Row:--T=', T, '----norm_groups:', norm_groups, '---NScale:', NScale, '---adjust:', adjustScale)
-                self.weight_normalization = ONINorm(T=T, norm_groups=norm_groups)
-            else: 
-               # print('ONI_Conv_Colum:--T=', T, '----norm_groups:', norm_groups, '---NScale:', NScale, '---adjust:', adjustScale)
-                self.weight_normalization = ONINorm_colum(T=T, norm_groups=norm_groups)
-        self.scale_ = torch.ones(out_channels, 1, 1, 1).fill_(NScale)
-        if adjustScale:
-            self.WNScale = Parameter(self.scale_)
-        else:
-           # self.scale = Variable(self.scale, requires_grad=False)
-            self.register_buffer('WNScale', self.scale_)
+        self.weight_normalization = ONINorm(T=T, norm_groups=1)
+        self.WNScale = 1.414 # Parameter([1.414])
 
     def forward(self, input_f: torch.Tensor) -> torch.Tensor:
         weight_q = self.weight_normalization(self.weight)
@@ -135,8 +61,21 @@ class ONI_Conv2d(torch.nn.Conv2d):
         out = F.conv2d(input_f, weight_q, self.bias, self.stride, self.padding, self.dilation, self.groups)
         return out
 
+class ONI_Linear(torch.nn.Linear):
+    def __init__(self, in_channels, out_channels, bias=True,
+                 T=7, norm_groups=1, norm_channels=0, NScale=1, adjustScale=False, *args, **kwargs):
+        super(ONI_Linear, self).__init__(in_channels, out_channels, bias)
+        self.weight_normalization = ONINorm(T=T, norm_groups=1)
+        self.WNScale = 1.414 
 
-Norm = _IdentityModule
+    def forward(self, input_f: torch.Tensor) -> torch.Tensor:
+        weight_q = self.weight_normalization(self.weight)
+        weight_q = weight_q * self.WNScale
+        out = F.linear(input_f, weight_q, self.bias)
+        return out
+
+
+# Norm = _IdentityModule
 NormConv = ONI_Conv2d
 
 
@@ -152,31 +91,30 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         #self.conv1 = conv3x3(inplanes, planes, stride)
         self.conv1 = NormConv(inplanes, planes, 3, stride, padding=1, bias=False)
-        #self.bn1 = nn.BatchNorm2d(planes)
-        self.bn1 = Norm(planes)
-        self.relu = nn.ReLU(inplace=True)
-        #self.conv2 = conv3x3(planes, planes)
-        #self.conv2 = Conv2d_ONI(planes, planes, 3, stride=1, padding=1, bias=False, T=7)
+        self.relu = nn.ReLU(inplace=True) 
         self.conv2 = NormConv(planes, planes, 3, stride=1, padding=1, bias=False)
-        #self.bn2 = nn.BatchNorm2d(planes)
-        self.bn2 = Norm(planes)
         self.downsample = downsample
         self.stride = stride
 
+        self.bias1a = nn.Parameter(torch.zeros(1))
+        self.bias1b = nn.Parameter(torch.zeros(1))
+        self.bias2a = nn.Parameter(torch.zeros(1))
+        self.bias2b = nn.Parameter(torch.zeros(1))
+        
+
     def forward(self, x):
-        residual = x
+        identity = x
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
+        out = self.conv1(x + self.bias1a)
+        out = self.relu(out + self.bias1b)
 
-        out = self.conv2(out)
-        out = self.bn2(out)
+        out = self.conv2(out + self.bias2a)
+        out = out + self.bias2b
 
         if self.downsample is not None:
-            residual = self.downsample(x)
+            identity = self.downsample(x + self.bias1a)
 
-        out += residual
+        out += identity
         out = self.relu(out)
 
         return out
@@ -187,45 +125,41 @@ class Bottleneck(nn.Module):
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
-        #self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.conv1 = NormConv(inplanes, planes, kernel_size=1, bias=False)
-        #self.bn1 = nn.BatchNorm2d(planes)
-        self.bn1 = Norm(planes)
-        
-        #self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.conv2 = NormConv(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        #self.bn2 = nn.BatchNorm2d(planes)
-        self.bn2 = Norm(planes)
-        
-        #self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
         self.conv3 = NormConv(planes, planes * 4, kernel_size=1, bias=False)
-        #self.bn3 = nn.BatchNorm2d(planes * 4)
-        self.bn3 = Norm(planes * 4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
 
+        self.bias1a = nn.Parameter(torch.zeros(1))
+        self.bias1b = nn.Parameter(torch.zeros(1))
+        self.bias2a = nn.Parameter(torch.zeros(1))
+        self.bias2b = nn.Parameter(torch.zeros(1))
+        self.bias3a = nn.Parameter(torch.zeros(1))
+        self.bias3b = nn.Parameter(torch.zeros(1))
+        
+
     def forward(self, x):
-        residual = x
+        identity = x
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
+        out = self.conv1(x + self.bias1a)
+        out = self.relu(out + self.bias1b)
 
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
+        out = self.conv2(out + self.bias2a)
+        out = self.relu(out + self.bias2b)
 
-        out = self.conv3(out)
-        out = self.bn3(out)
+        out = self.conv3(out + self.bias3a)
+        out = out  + self.bias3b
 
         if self.downsample is not None:
-            residual = self.downsample(x)
+            identity = self.downsample(x + self.bias1a)
 
-        out += residual
+        out += identity
         out = self.relu(out)
 
         return out
+
 
 
 class ResNetDebug(nn.Module):
@@ -235,23 +169,26 @@ class ResNetDebug(nn.Module):
         super(ResNetDebug, self).__init__()
         #self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.conv1 = NormConv(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = Norm(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        # self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = ONI_Linear(512 * block.expansion, num_classes, adjustScale=True)
+        self.bias1 = nn.Parameter(torch.zeros(1))
+        self.bias2 = nn.Parameter(torch.zeros(1))
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+                # n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                # m.weight.data.normal_(0, math.sqrt(2. / n))
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            # elif isinstance(m, nn.BatchNorm2d):
+            #     m.weight.data.fill_(1)
+            #     m.bias.data.zero_()
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
@@ -259,8 +196,7 @@ class ResNetDebug(nn.Module):
             downsample = nn.Sequential(
                 #nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
                 NormConv(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False, ONIRow_Fix=True),
-                #nn.BatchNorm2d(planes * block.expansion), )
-                Norm(planes * block.expansion), )
+               )
 
         layers = []
         layers.append(block(self.inplanes, planes, stride, downsample))
@@ -272,8 +208,7 @@ class ResNetDebug(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
+        x = self.relu(x + self.bias1)
         x = self.maxpool(x)
 
         x = self.layer1(x)
@@ -283,7 +218,7 @@ class ResNetDebug(nn.Module):
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.fc(x + self.bias2)
 
         return x
 
